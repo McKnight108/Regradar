18a69b03efff8ea7726166dab6e2c6672c818583
Chan <chenmk7@163.com>
2026-04-14 11:16:40 +0800

    4/14

--- 最小二乘拟合kdp.py
+++ 最小二乘拟合kdp.py
@@ -14,13 +14,8 @@ for i in range(half_w, len(phi) - half_w):
     r_win   = r[i - half_w:i + half_w + 1]
     phi_win = phi[i - half_w:i + half_w + 1]
 
-    # 跳过窗口内存在 NaN 的点
-    mask = np.isfinite(phi_win)
-    if mask.sum() < half_w:      # 有效点太少则跳过
-        continue
-
-    A = np.vstack([r_win[mask], np.ones(mask.sum())]).T
-    a, _ = np.linalg.lstsq(A, phi_win[mask], rcond=None)[0]
+    A = np.vstack([r_win, np.ones(len(r_win))]).T
+    a, _ = np.linalg.lstsq(A, phi_win, rcond=None)[0]
 
     kdp_lsf[i] = 0.5 * a
 
